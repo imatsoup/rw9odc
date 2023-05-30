bb969fd31433507410b2974ee718de94b536cb11
imatsoup <93290208+imatsoup@users.noreply.github.com>
2023-05-30 18:56:54 -0400

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -10,7 +10,19 @@ export class CartComponent {
   
   items = this.cartService.getItems();
 
-  constructor( private cartService: CartService ){
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
+  constructor( 
+    private cartService: CartService, 
+    private formBuilder: FormBuilder,
+    ){  }
+  
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted.', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 }
